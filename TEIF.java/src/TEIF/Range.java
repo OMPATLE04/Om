301e412bd99e250a578f3864ae63d3d9b94bcaaa
schar <schar@OM>
2025-12-17 16:28:24 +0530

    no. changes while storing

--- TEIF.java/src/TEIF/Range.java
+++ TEIF.java/src/TEIF/Range.java
@@ -2,8 +2,15 @@ package TEIF;
 
 public class Range {
 	public static void main (String[] args) {
-		short a = 0b1011;
+		int  a = 45;
 		System.out.println(a);
+		int  b = 0b1011;
+		System.out.println(b);
+		int  c = 0x45;
+		System.out.println(c);
+		int  d = 045;
+		System.out.println(d);
+		
 	}
 	
 
