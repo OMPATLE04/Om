e6e198bef2054a719033d84e49d0c1a5aa15e142
schar <schar@192.168.1.9>
2025-12-20 07:54:44 +0530

    hard code for 1D array

---
+++ TEIF.java/src/Arrays/marks.java
@@ -0,0 +1,25 @@
+package Arrays;
+import java.util.*;
+
+public class marks {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		int a[]= new int[5];
+		System.out.println("enter marks of student"  );
+		a[0]=sc.nextInt();
+		a[1]=sc.nextInt();
+		a[2]=sc.nextInt();
+		a[3]=sc.nextInt();
+		a[4]=sc.nextInt();
+		System.out.println("these are the mark of students" );
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+			
+		
+		
+	}
+
+}
