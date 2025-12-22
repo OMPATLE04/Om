d52e03fb7e1fd282d407c30703ed854dfe95af69
schar <schar@192.168.1.7>
2025-12-22 22:21:51 +0530

    code to convert char to int

---
+++ TEIF.java/src/strings/convert_CharToInt.java
@@ -0,0 +1,14 @@
+package strings;
+import java.util.*;
+
+public class convert_CharToInt {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter any alphabet");
+		char ch = sc.next().charAt(0);
+		int b=ch;
+		System.out.println(b);
+		
+	}
+
+}
