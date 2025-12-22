2e35d51a3db6462be4995c5c36d4292c702a1e1f
schar <schar@192.168.1.7>
2025-12-22 22:22:25 +0530

    use of lowercase and uppercase fn

---
+++ TEIF.java/src/strings/fn_Upper_LowerCase.java
@@ -0,0 +1,18 @@
+package strings;
+
+public class fn_Upper_LowerCase {
+	public static void main(String []args) {
+		String s1 = "Arsh";
+		System.out.println(s1.toUpperCase());
+		System.out.println(s1.toLowerCase());
+		System.out.println(s1.contains("Ar"));
+		System.out.println(s1.contains("i"));
+		System.out.println(s1.indexOf("A"));
+		System.out.println(s1.substring(3));
+		System.out.println(s1.substring(0,2));
+		System.out.println(s1.charAt(3));
+		
+		
+	}
+
+}
