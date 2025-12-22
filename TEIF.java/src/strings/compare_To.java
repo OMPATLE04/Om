31a294d7072520c6badce00871b005524ea7fd50
schar <schar@192.168.1.7>
2025-12-22 22:21:13 +0530

    use of compare_To

---
+++ TEIF.java/src/strings/compare_To.java
@@ -0,0 +1,23 @@
+package strings;
+import java.util.*;
+
+public class compare_To {
+	public static void main(String []args) {
+		String s1 = "SACHIN";
+		String s2 = "SAURAV";
+		int res = s1.compareTo(s2);
+		System.out.println(res);
+		if(res>0) {
+			System.out.println("s1 is greater");
+			
+		}else if (res<0) {
+			System.out.println("s2 is greater");
+		}
+		else {
+			System.out.println("both are equal");
+		}
+		
+		
+	}
+
+}
