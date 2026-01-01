e53c47cb6b289d7d442ceb303f6d8ea2e91f6ea2
schar <schar@192.168.1.5>
2026-01-01 23:42:26 +0530

    compiler error

---
+++ TEIF.java/src/Encapsulation/compile_err.java
@@ -0,0 +1,44 @@
+package Encapsulation;
+
+public class compile_err {
+public static void main (String []args) {
+		
+		s_Employee e = new s_Employee( );
+		System.out.println(e.getName());
+		System.out.println(e.getId());
+		System.out.println(e.getSallary());
+		
+		}
+
+	}
+	class s_Employee{ 
+		private String Name;
+		private int Id;
+		private int Sallary;
+		
+		public s_Employee(String Name , int Id  , int Sallary) {
+			 this.Name = Name;			this.Id = Id;
+			this.Sallary = Sallary;
+		}
+		
+		public String getName(){
+			return Name;
+		}
+		public int getId(){
+			return Id;
+		}
+		public int getSallary(){
+			return Sallary;
+		}
+	}
+
+// in the above program the compiler will look for such a constructor which does not accept any argument 
+//but their is no such constructor present in their class and hence we are going to get a compile time error.
+//
+//
+//only and onlu if the , programmer hasn't define a constructor in the class the compiler will 
+//add zero(0) paramerterize constructor. 
+//
+//
+//such a constructor which is provided by the compiler when no constructor  was provided by the 
+//programmmer is known as default constructoe.
